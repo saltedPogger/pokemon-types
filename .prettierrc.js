4189f8b1c5327f4e0944b39a684bb3e38bf22a89
Adrien E <cotontigeh@gmail.com>
2022-11-27 02:15:34 +0100

    feat: init next & tailwind

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 90,
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
+import { Roboto } from '@next/font/google'
+
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+const robotoFont = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--font-roboto',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${robotoFont.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
